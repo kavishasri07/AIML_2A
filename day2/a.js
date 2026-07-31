adc9ccf628e0689af53bfb793c07d0757a36f2c4
Kavisha Srivastava <kavishasri07@gmail.com>
2026-07-31 11:19:27 +0530

    Day 2

--- day2/a.js
+++ day2/a.js
@@ -2,3 +2,10 @@ console.log("===== Variable =====");
 var fname= "Kavisha";
 var sname= "Srivastava";
 console.log(fname + " " + sname);
+var a= 40;
+var b= 20;
+if (a>b) {
+    console.log("A is greater, value of A= ", a);
+} else {
+    console.log("B is greater, value of B= ", b);
+}
