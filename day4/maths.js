e12defeb22976e1805ef2ede7fa14ea03015393a
Kavisha Srivastava <kavishasri07@gmail.com>
2026-08-19 14:26:27 +0530

    Day 4

---
+++ day4/maths.js
@@ -0,0 +1,7 @@
+console.log("Maximum: ", Math.max(20, 23, 40, 1, 59, 0));
+console.log("Minimum: ", Math.min(20, 23, 1, 59, 0));
+console.log("Round: ", Math.round(4.6));
+console.log("Floor: ", Math.floor(4.8));
+console.log("Ceil: ", Math.ceil(4.2));
+console.log("Square root: ", Math.sqrt(2));
+console.log("Power: ", Math.pow(3, 2));
